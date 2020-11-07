b6e78ab48de8da17ebf58b4bed93194c0055ab55
Matthew Cvek <matthew.cvek@centrality.ai>
2020-11-08 10:43:42 +1300

    add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { NavBar } from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
+        <NavBar />
       </header>
     </div>
   );
